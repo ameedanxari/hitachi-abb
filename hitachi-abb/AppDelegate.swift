5edf5a38ba3df3a289c65d75ca01a237af80082e
macintosh <ameed.ansari@creatrixe.com>
2020-12-21 15:56:01 -0800

    Project initialized

---
+++ hitachi-abb/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  hitachi-abb
+//
+//  Created by macintosh on 2020-12-21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ hitachi-abb/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  hitachi-abb
+//
+//  Created by macintosh on 2020-12-21.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+}
+
